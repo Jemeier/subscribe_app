b5dd942f022d60103d2fd1f74c3e8e6ae0959255
Anthony McGovern <mcgovern.anthony@gmail.com>
2015-08-03 17:05:27 -0400

    edit routes, add dual subscription routes, explore & dashboard named routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,15 @@ Rails.application.routes.draw do
 
   resources :subscriptions
 
+
   root to: "users#home"
+  
+  get 'explore'   => 'subscriptions#index'
+  get 'dashboard' => 'subscriptions#index'
+  
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
 
   # Example of regular route:
