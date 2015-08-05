9a825d382009a41728004dddf7550f2cfa641c05
Andy Agus <Redagus@gmail.com>
2015-08-05 18:11:30 -0400

    added paperclip upload functionality to seeds file

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'paperclip'
 gem 'devise'
-gem 'aws-sdk'
+gem 'aws-sdk', '~> 1.61.0'
 gem 'bourbon'
 gem 'sass'
 gem 'neat'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
 
-
-user = User.new
-user.email = 'test@example.com'
-user.password = 'foobar44'
-user.save!
+# user = User.new
+# user.email = 'test@example.com'
+# user.password = 'foobar44'
+# user.save!
 
 # creates a mock subscription for each of the products currently in the database
 if Subscription.count < 463
@@ -17,6 +16,15 @@ if Subscription.count < 463
                           product_id: (n+1))
   end
 end
+
+products = Product.all
+
+products.each do |product|
+   product.image = File.open("./wp/all_photos/photo#{product.image_num}.jpeg","rb")
+   product.save
+end
+
+
 
 
 # products = Product.all
